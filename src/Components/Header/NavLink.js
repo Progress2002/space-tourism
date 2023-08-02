193b83f40aedb2098da532cc2f4fe58588f281b4
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-08-02 23:58:42 +0100

    Add isActive  prop to Navlink

--- src/Components/Header/NavLink.js
+++ src/Components/Header/NavLink.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavLink = (props) => {
-  const { address, span, value } = props;
+  const { address, span, value, isActive } = props;
   return (
     <li className=" md:py-10 cursor-pointer border-primaryColor tracking-widest">
-      <Link to={address} className="lg:py-9 md:hover:border-b-4">
+      <Link to={address}className={isActive(address)
+         ? 'lg:py-9 md:hover:border-b-4 border-b-4' : 'lg:py-9 md:hover:border-b-4' }>
         <span className="mr-3 font-bold md:hidden lg:inline">{span}</span>
         {value}
       </Link>
